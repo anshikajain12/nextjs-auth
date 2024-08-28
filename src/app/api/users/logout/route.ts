bf165a45919f32a5c1d601b1bb60431cc0571d65
anshika <nisthajain2412@gmail.com>
2024-08-28 23:48:02 +0530

    verify, loging, logout added

---
+++ src/app/api/users/logout/route.ts
@@ -0,0 +1,20 @@
+import { connect } from "@/dbConfig/dbConfig";
+import { NextResponse } from "next/server";
+
+connect();
+export async function GET(request: NextResponse) {
+    try {
+        const response = NextResponse.json({
+            message: "Loggout Successfully",
+            success: true
+        });
+        response.cookies.set("token", "", {
+            httpOnly: true,
+            expires: new Date(0)
+        })
+        return response;
+
+    } catch (error: any) {
+        return NextResponse.json({ error: error.message }, { status: 500 })
+    }
+}
