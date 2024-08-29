93c158365c8de413a240871c027a43032725383b
anshika <nisthajain2412@gmail.com>
2024-08-29 06:50:45 +0530

    profile route added

---
+++ src/app/api/users/me/route.ts
@@ -0,0 +1,16 @@
+import { connect } from "@/dbConfig/dbConfig";
+import User from "@/models/userModel";
+import { NextRequest, NextResponse } from "next/server";
+import { getDataFromToken } from "@/helpers/getDataFromToken";
+
+connect();
+export async function POST(request: NextRequest) {
+    //extract data from token
+    const decodedTokenUserId = await getDataFromToken(request);
+    const user = User.findOne({ _id: decodedTokenUserId }).select("-password");
+    //check if there is no user
+    return NextResponse.json({
+        message: "User found",
+        data: user
+    })
+}
